bba0afc66d56f433bbb17d557a53edabf462678d
cherrysmp-store <playercarrermode1@gmail.com>
2026-03-24 22:44:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ function closePayment(){
 }
 
 function payPal(){
-  window.open("https://www.paypal.me/TimCherry000","_blank");
+  window.open("https://www.paypal.com/paypalme/TimCherry000","_blank");
 }
 
 function bankTransfer(){
